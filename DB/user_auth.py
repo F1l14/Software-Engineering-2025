b0dceb640ee3f1151f548ef85975544c8299a360
F1l14 <filipmn42@gmail.com>
2025-02-23 13:03:22 +0200

    working user registration with prevention for existing username

--- DB/user_auth.py
+++ DB/user_auth.py
@@ -8,3 +8,13 @@ def login(cursor):
         print("Login successful")
     else:
         print("Wrong cresecentials")
+
+def register(cursor, db):
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    try:
+        cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        db.commit()
+        print("User registered successfully")
+    except Exception as e:
+        print(f"An error occured: {e}")
