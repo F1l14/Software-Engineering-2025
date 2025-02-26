0e67f7ef8a4a2ec3caee94d118c2de91852f9eed
F1l14 <filipmn42@gmail.com>
2025-02-26 20:45:16 +0200

    with successful login a 8 byte hex token is generated and stored in the db. With every new login the last token is replaced with a new one. The logged in user is stored in a dictionary in the main script

--- DB/user_auth.py
+++ DB/user_auth.py
@@ -1,13 +1,23 @@
-# cursor.execute("SELECT * FROM %s" % ("users"))
+from os import urandom
 
-def login(cursor):
+
+def login(cursor, db):
     username = input("Enter username: ")
     password = input("Enter password: ")
     cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
     if cursor.fetchone():
+        token =  generate_token()
+        cursor.execute("DELETE FROM user_tokens WHERE username = %s", (username,))
+        db.commit()
+        cursor.execute("INSERT INTO user_tokens (username, token) VALUES (%s, %s)", (username, token))
+        db.commit()
+        
         print("Login successful")
+        # return a dictionary with username and token
+        return {username:username, token:token}
     else:
         print("Wrong cresecentials")
+        return None
 
 def register(cursor, db):
     username = input("Enter username: ")
@@ -18,3 +28,6 @@ def register(cursor, db):
         print("User registered successfully")
     except Exception as e:
         print(f"An error occured: {e}")
+
+def generate_token():
+    return urandom(8).hex()
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import DB.user_auth as auth
 import DB.conn as conn
 
+current_user = None
 # When a script is run directly from the command line, the special variable __name__ is set to __main__.
 # This is not the case when the script is imported as a module.
 if __name__=="__main__":
     try:
         db=conn.connect()
         cursor = db.cursor()
-        # auth.login(cursor)
-        auth.register(cursor, db)
+        current_user=auth.login(cursor, db)
+        #auth.register(cursor, db)
+
+        print(current_user)
+        print(type(current_user))
     except Exception as e:
         print(f"An error occured: {e}")
 
