e2eb4078bfa8c64ac3ca615b07879bbfefe882df
F1l14 <filipmn42@gmail.com>
2025-05-19 18:40:41 +0300

    create user method and connector update

--- src/Class/DBManager.py
+++ src/Class/DBManager.py
@@ -1,10 +1,23 @@
 import mysql.connector
 class DBManager:
-    def __init__(self):
+    def __init__(self, host="localhost", user="root", password="", database="LinQ-SEProject"):
         self.conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="LinQ-SEProject"
+            host=host,
+            user=user,
+            password=password,
+            database=database
         )
+    def  close(self):
+        self.conn.close()
 
+    def create_user(self, username, password, firstname, lastname):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("INSERT INTO users (username, password, firstname, lastname) VALUES (%s, %s, %s, %s)", (username, password, firstname, lastname))
+            self.conn.commit()
+        except mysql.connector.Error as err:
+            return f"Error: {err}"
+        else:
+            return "User created successfully"
+        finally:
+            cursor.close()
