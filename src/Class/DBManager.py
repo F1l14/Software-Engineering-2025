4223251942002df526296caca36b13cb5216b4ef
F1l14 <filipmn42@gmail.com>
2025-05-19 18:46:13 +0300

    create business method

--- src/Class/DBManager.py
+++ src/Class/DBManager.py
@@ -21,3 +21,15 @@ class DBManager:
             return "User created successfully"
         finally:
             cursor.close()
+
+    def create_business(self, name, owner, logo=None):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("INSERT INTO business (name, owner, logo) VALUES (%s, %s, %s)", (name, owner, logo))
+            self.conn.commit()
+        except mysql.connector.Error as err:
+            return f"Error: {err}"
+        else:
+            return "Business created successfully"
+        finally:
+            cursor.close()
