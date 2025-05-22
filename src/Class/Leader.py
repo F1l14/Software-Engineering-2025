c8ce58dc0b92da1f2ba0af1d01c5a6cefda5a01c
F1l14 <filipmn42@gmail.com>
2025-05-22 15:28:05 +0300

    New: added __task_list attribute and implemented createTask method in Leader class

--- src/Class/Leader.py
+++ src/Class/Leader.py
@@ -1,5 +1,6 @@
-from src.Class import User
+from src.Class import User, Task
 class Leader(User):
+    __task_list = []
     def __init__(self, username, firstname, lastname):
         super().__init__(username, firstname, lastname)
         self.__team = None
@@ -8,3 +9,11 @@ class Leader(User):
 
     def __str__(self):
         return f"Leader({self.username}, {self.firstname}, {self.lastname})"
+    
+    def getProjects(self, db):
+        return db.queryProjects(self.username)
+    
+    def createTask(self, db, team_id, name, assigned_to=None):
+        new_task = Task(name, assigned_to)
+        self.__task_list.append(new_task)
+        return {"db_message": db.createTask(team_id, name, assigned_to), "task": new_task}
