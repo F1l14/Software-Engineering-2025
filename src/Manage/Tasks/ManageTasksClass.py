3390c27f120ed1f772dafa8913672ad891d23b3b
F1l14 <filipmn42@gmail.com>
2025-05-22 15:56:13 +0300

    Refactor ManageTasksClass to append tasks and projects to internal lists and update createTask method to use user object

--- src/Manage/Tasks/ManageTasksClass.py
+++ src/Manage/Tasks/ManageTasksClass.py
@@ -1,7 +1,15 @@
 from src.Class.DBManager import DBManager
 class ManageTasksClass:
+    """
+
+    Create Tasks and Projects ???
+
+    """
     __db = None
     __user = None
+    __tasks_list = []
+    __projects_list = []
+
     def __init__(self, user):
         # self.progress_screen = ProgressScreen()
         # self.progress_screen.manage = self
@@ -11,12 +19,23 @@ class ManageTasksClass:
         
     def getTasks(self):
         tasks = self.__user.getTasks(self.__db)
+        self.__tasks_list.append(tasks)
         print(tasks)
 
     def getProjects(self):
+        # user = leader
         projects = self.__user.getProjects(self.__db)
+        self.__projects_list.append(projects)
         print(projects)
 
+    def completeTask(self, task_id):
+        # user = employee
+        message = self.__user.completeTask(self.__db, task_id)
+        print(message)
+
     def createTask(self, team_id, name, assigned_to=None):
-        task = self.__db.createTask(team_id, name, assigned_to)
+        # user = leader
+        task = self.__user.createTask(self.__db, team_id, name, assigned_to)
         print(task.message)
+
+    
