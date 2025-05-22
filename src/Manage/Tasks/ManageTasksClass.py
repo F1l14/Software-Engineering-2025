5af8615de46aa794f460b419192105833caa22e2
F1l14 <filipmn42@gmail.com>
2025-05-22 15:06:22 +0300

    New: method: getTasks

---
+++ src/Manage/Tasks/ManageTasksClass.py
@@ -0,0 +1,14 @@
+from src.Class.DBManager import DBManager
+class ManageTasksClass:
+    __db = None
+    __username = None
+    def __init__(self, username):
+        # self.progress_screen = ProgressScreen()
+        # self.progress_screen.manage = self
+        # self.progress_screen.display()
+        self.__db = DBManager()
+        self.__username = username
+        
+    def getTasks(self):
+        tasks = self.__db.queryTasks(self.__username)
+        print(tasks)
