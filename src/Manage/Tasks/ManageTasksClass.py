b5c4b6c76f62ba5c90623ab71eca43b968be6f39
F1l14 <filipmn42@gmail.com>
2025-05-24 11:44:11 +0300

    New: Leader can now access the Task Creation Screen

--- src/Manage/Tasks/ManageTasksClass.py
+++ src/Manage/Tasks/ManageTasksClass.py
@@ -1,13 +1,10 @@
 from src.Class.DBManager import DBManager
 from src.Screen.Tasks.TasksScreen import TasksScreen
+from src.Screen.Tasks.TaskCreationScreen import TaskCreationScreen
 from src.Class.TaskListWidget import TaskItem
 from PyQt6.QtWidgets import QListWidgetItem
+from PyQt6.QtWidgets import QMessageBox
 class ManageTasksClass:
-    """
-
-    Create Tasks and Projects ???
-
-    """
     __db = None
     __user = None
     __tasks_list = {}
@@ -21,7 +18,23 @@ class ManageTasksClass:
 
         self.getTasks()
         self.displayTasks()
+        self.tasks_screen.create_button.clicked.connect(self.newCreateTaskScreen)
+    
+
+    def newCreateTaskScreen(self):
+        # user = leader
+        if not self.checkLeader():
+            print("You are not a leader")
+            self.show_popup()
+            return
+        self.create_task_screen = TaskCreationScreen()
+    
+    def addProjectsToTree(self):
         
+        self.getProjects()
+        for project in self.__projects_list:
+            self.tasks_screen.treeWidget.addTopLevelItem(project)
+
     def getTasks(self):
         tasks = self.__user.getTasks(self.__db)
         for task in tasks:
@@ -44,10 +57,7 @@ class ManageTasksClass:
                 
 
     def getProjects(self):
-        # user = leader
-        if not self.checkLeader():
-            print("You are not a leader")
-            return
+        
         projects = self.__user.getProjects(self.__db)
         self.__projects_list.append(projects)
         print(projects)
@@ -84,3 +94,11 @@ class ManageTasksClass:
         self.tasks_screen.listWidget.addItem(item)
         self.tasks_screen.listWidget.setItemWidget(item, task_widget)
         # self.tasks_screen.show()
+
+    def show_popup(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Error")
+        msg.setText("You are not a Leader!")
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msg.exec()
