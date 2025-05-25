0009c9f4618485c6f3dbdd52f2f0f284d248aaa0
zoubou <your.jzoub03@gmail.com>
2025-05-26 00:31:55 +0300

    added saveNewProject method to manageproject class

--- src/Manage/Projects/ManageProjectClass.py
+++ src/Manage/Projects/ManageProjectClass.py
@@ -1,5 +1,6 @@
 from src.Screen.Projects.ProjectScreen import ProjectScreen
 from src.Screen.Projects.ProjectCreationScreen import ProjectCreationScreen
+from src.Screen.Projects.ProjectAssignScreen import ProjectAssignScreen
 from src.Class.DBManager import DBManager
 
 class ManageProjectClass:
@@ -17,8 +18,21 @@ class ManageProjectClass:
         db = DBManager()
         departments = db.queryAllDepartments()
         return departments
+    
+    def saveNewProject(self, selected_names, projectDetails):
+        db = DBManager()
+        message = db.createProject(selected_names, projectDetails)
+        print(message)
+        self.project_screen.showAllProjects()
 
     def showProjectCreation(self):
         self.projectCreation_screen = ProjectCreationScreen()
         self.projectCreation_screen.manage = self
         self.projectCreation_screen.display()
+
+    def showAssignProjectScreen(self, projectDetails):
+        self.projectAssign_screen = ProjectAssignScreen(projectDetails)
+        self.projectAssign_screen.manage = self
+        self.projectAssign_screen.display()
+
+    
