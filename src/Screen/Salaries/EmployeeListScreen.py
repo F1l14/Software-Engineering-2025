ecc308a3a99b0c7b87a11af19836deca86926add
zoubou <your.jzoub03@gmail.com>
2025-05-27 11:19:09 +0300

    added showEmployeeSalariesList to employeeListScreen and ui  changes

--- src/Screen/Salaries/EmployeeListScreen.py
+++ src/Screen/Salaries/EmployeeListScreen.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QTableWidget, QTableWidgetItem, QMessageBox
 from PyQt6 import uic
 
 class EmployeeListScreen(QDialog):
@@ -7,4 +7,20 @@ class EmployeeListScreen(QDialog):
     
     def display(self):
         uic.loadUi("ui/7_Salaries/EmployeeListScreen.ui", self)
+        self.salaryList = self.findChild(QTableWidget, "employeeSalaryList")
+        self.showEmployeeSalariesList()
         self.exec()
+
+    def showEmployeeSalariesList(self):
+        self.salaries = self.manage.getEmployeeSalaries()
+        username = [item[0] for item in self.salaries]
+        salary = [item[1] for item in self.salaries]
+
+        self.salaryList.setRowCount(len(username))
+        self.salaryList.setColumnCount(2)
+        self.salaryList.setHorizontalHeaderLabels(['username', 'salary'])
+
+        for row, (username, salary) in enumerate(zip(username, salary)):
+            self.salaryList.setItem(row, 0, QTableWidgetItem(str(username)))
+            self.salaryList.setItem(row, 1, QTableWidgetItem(str(salary)))
+            
