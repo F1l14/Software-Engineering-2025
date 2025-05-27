a6fd239d1474c5d8195fbbda87b89b1a572b5c66
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2025-05-27 19:10:50 +0300

    remove unused queryAllProjects method from DBManager

--- src/Class/DBManager.py
+++ src/Class/DBManager.py
@@ -217,17 +217,6 @@ class DBManager:
         finally:
             cursor.close()       
 
-    #Use Case 2:
-    def queryAllProjects(self):
-        cursor = self.conn.cursor()
-        try:
-            cursor.execute("SELECT * FROM projects")
-            result = cursor.fetchall()
-            return result
-        except mysql.connector.Error as err:
-            return f"Error: {err}"
-        finally:
-            cursor.close()       
 
             
     # Use Case 3:
