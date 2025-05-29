68b3ebd679613efc2f6ca026f3d0e19732a14ce3
zoubou <your.jzoub03@gmail.com>
2025-05-30 02:26:36 +0300

    added saveBonusDetails method

--- src/Manage/Salaries/ManageBonusClass.py
+++ src/Manage/Salaries/ManageBonusClass.py
@@ -1,14 +1,12 @@
-#from src.Screen.Salaries.EmployeePercentScreen import EmployeePercentScreen
+from PyQt6.QtWidgets import QMessageBox
 from src.Screen.Salaries.BonusSetupScreen import BonusSetupScreen
-#from src.Screen.Salaries.BonusBoardScreen import BonusBoardScreen
+from src.Screen.Salaries.BonusBoardScreen import BonusBoardScreen
 from src.Class.DBManager import DBManager
 
 class ManageBonusClass:
     def __init__(self):
-        #self.employeePercent_screen = EmployeePercentScreen()
         self.bonusSetup_screen = BonusSetupScreen()
-        #self.bonusBoard_screen = BonusBoardScreen()
-        self.employeePercent_screen.manage = self
+        self.bonusBoard_screen = BonusBoardScreen()
         self.bonusSetup_screen.manage = self
         self.bonusBoard_screen.manage = self
 
@@ -22,3 +20,20 @@ class ManageBonusClass:
         db = DBManager()
         BonusState = db.checkBonusState()
         return BonusState
+    
+    def saveBonusDetails(self, badget_numbers, perc1, perc2):
+        db = DBManager()
+        message = db.createBonusDetails(badget_numbers, perc1, perc2)
+        print(message)
+        try:
+            self.bonusBoard_screen.display()
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            QMessageBox.critical(None, "Error", f"Error during display: {repr(e)}")
+
+
+    def getBonusBoardDetails(self):
+        db = DBManager()
+        return db.queryBonusBoardDetails()
+        
