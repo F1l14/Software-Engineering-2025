9de842875b842b5b0c6097f845da4cc224bfd4b6
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2025-05-30 19:50:30 +0300

    refactor: Simplify visibility logic for buttons based on user roles in MainScreen

--- src/Screen/MainScreen.py
+++ src/Screen/MainScreen.py
@@ -20,11 +20,19 @@ class MainScreen(QMainWindow):
         self.tasksButton.clicked.connect(self.manage.showTasksScreen)
         self.leavesButton.clicked.connect(self.manage.showLeavesMenu)
         
-        if Session.getRole() != "admin":
-            self.progressButton.setVisible(False)
-            
-        if Session.getRole() == "employee":
+        if Session.getRole() == "employee": # Hide the projects button for employees
             self.projectsButton.setVisible(False)
+        
+        if Session.getRole() != "admin":   # Hide the evaluation and salaries button for employees and managers
+            self.progressButton.setVisible(False)
+            self.salariesButton.setVisible(False)
+        
+        
+        if Session.getRole() != "employee": # Hide the tasks and teams button for admin and managers
+            self.tasksButton.setVisible(False)
+            self.teamsButton.setVisible(False)
+            
+
         if Session.getRole() == "admin":
             self.evaluationButton.setEnabled(True)
             self.leavesButton.setVisible(False)
